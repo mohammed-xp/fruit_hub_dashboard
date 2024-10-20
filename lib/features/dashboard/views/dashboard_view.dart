ecdf3470bd04a0e389de74a7708b62e727f88ef6
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-10-20 21:32:09 +0200

    Initial Commit

---
+++ lib/features/dashboard/views/dashboard_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  static const routeName = 'dashboard';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
